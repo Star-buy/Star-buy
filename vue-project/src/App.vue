4ace41bbe8f6ed0eb030b69f3146e956d204e32e
Fares-sghaier# <faroussassg007@gmail.com>
2022-04-19 16:53:01 +0100

    working on Navbar

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,30 +1,14 @@
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
+import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+<main> <Navbar/></main>
 </template>
 
 <style>
-@import "./assets/base.css";
+/* @import "./assets/base.css";
 
 #app {
   max-width: 1280px;
@@ -83,5 +67,5 @@ a,
   .logo {
     margin: 0 2rem 0 0;
   }
-}
+} */
 </style>
